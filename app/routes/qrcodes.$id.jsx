b1bac84ee6f4c42408f3c30221d7b6445d9890e4
Jaco Swarts <jaco@vatfree.com>
2024-05-06 16:14:37 +0200

    WIP: tutorial

---
+++ app/routes/qrcodes.$id.jsx
@@ -0,0 +1,18 @@
+import { json } from "@remix-run/node";
+import invariant from "tiny-invariant";
+import { getQRCodeImage } from "../models/QRCode.server";
+import db from "../db.server";
+
+export const loader = async ({ params }) => {
+  invariant(params.id, "Could not find QR code destination");
+
+  const id = Number(params.id);
+  const qrCode = await db.qRCode.findFirst({ where: { id } });
+
+  invariant(qrCode, "Could not find QR code destination");
+
+  return json({
+    title: qrCode.title,
+    image: await getQRCodeImage(id),
+  });
+};
