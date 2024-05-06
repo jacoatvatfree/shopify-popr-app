e8a36a978165f62585e9b6af07eeb8e6a818eb22
Jaco Swarts <jaco@vatfree.com>
2024-05-06 21:28:37 +0200

    Formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,26 @@
 /** @type {import('@types/eslint').Linter.BaseConfig} */
 module.exports = {
-  root: true,
-  extends: [
-    "@remix-run/eslint-config",
-    "@remix-run/eslint-config/node",
-    "@remix-run/eslint-config/jest-testing-library",
-    "prettier",
-  ],
-  globals: {
-    shopify: "readonly",
-  },
-  env: {
-    browser: true,
-    es6: true,
-    mocha: true,
-  },
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  rules: {},
-};
+	root: true,
+	extends: [
+		"@remix-run/eslint-config",
+		"@remix-run/eslint-config/node",
+		"@remix-run/eslint-config/jest-testing-library",
+		"prettier",
+	],
+	globals: {
+		shopify: "readonly",
+	},
+	env: {
+		browser: true,
+		es6: true,
+		mocha: true,
+	},
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	rules: {},
+}
